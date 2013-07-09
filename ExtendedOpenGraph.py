82bee5a4c80357713c97ed687f7fa08cf16ba77c
AhnSeongHyun <sh84.ahn@gmail.com>
2013-07-09 15:27:37 +0900

    add getPresentImage()
    to get PresentImage using BeautifulSoup Lib

--- ExtendedOpenGraph.py
+++ ExtendedOpenGraph.py
@@ -2,8 +2,11 @@
  
 import opengraph
 import re
-import urllib2 
-
+import urllib2
+try:
+    from bs4 import BeautifulSoup
+except ImportError:
+    from BeautifulSoup import BeautifulSoup
 
 
 def parse(url):
@@ -12,8 +15,20 @@ def parse(url):
 	 
 
 	if ogpResult.is_valid() == True:
-		pass
-		#통과를 해도 title, image, url 이 없으면 해당의 것을 찾아야 한다.  
+		
+		if ogpResult['_url'] is not None:
+			parseResult['url'] = ogpResult['_url']
+		
+		if ogpResult['image'] is not None:
+			parseResult['image'] = ogpResult['image']
+		 
+		if ogpResult['title'] is not None:
+			parseResult['title'] = ogpResult['title']
+
+		if ogpResult['type'] is not None:
+			parseResult['type'] = ogpResult['type']
+		
+
 	else:
 		parseResult = parseNonVaildUrl(url, parseResult)
 
@@ -30,14 +45,31 @@ def parseNonVaildUrl(url, parseResult):
 	parseResult['title'] = getTitle(html)
 
 	#첫번째 이미지를 가져온다. 
-	parseResult['image'] = getPresentImage(url)
+	parseResult['image'] = getPresentImage(html)
 
 	#url을 가져온다.  
 	parseResult['url'] = getUrl(url)
+
+	#type을 가져온다. 
+	parseResult['type'] = getType(html)
+
 	return parseResult; 
 
-def getPresentImage(url):
+
+def getType(html):
 	return None
+
+def getPresentImage(html):
+
+	presntImage =""
+	soup = BeautifulSoup(html)
+	images = soup.html.find_all('img')
+	
+	for image in images: 
+		if '.gif' not in image['src']:
+			presntImage = image['src'] #first image
+
+	return presntImage
 
 
 def getUrl(url):
@@ -63,8 +95,11 @@ def getHtml(url):
 
 
 if __name__ == '__main__':
-	#url = "http://www.youtube.com/watch?v=q3ixBmDzylQ"
-	#print(parse(url)) 
+	
+	print "youytube"
+	url1 = "http://www.youtube.com/watch?v=q3ixBmDzylQ"
+	print(parse(url1)) 
 
-	url = "http://ash84.tistory.com"
-	print(parse(url))
+	print "tistory"
+	url2 = "http://indf.tistory.com/"
+	print(parse(url2))
